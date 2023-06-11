a3f50507df57c9b94a17d1c68ee6d0c1fc805c34
jaime <jaimegonicarnicero@hotmail.com>
2023-06-11 09:23:13 +0200

    Refactor of UploadImageModal.jsx

--- src/ui/components/UploadImageModal/UploadImageModal.jsx
+++ src/ui/components/UploadImageModal/UploadImageModal.jsx
@@ -11,6 +11,9 @@ import { imageToBase64 } from "../../../core/services/ImageToBase64/ImageToBase6
 import { extractOriginalImageSize } from "../../../core/services/ExtractOriginalImageSize/ExtractOriginalImageSize";
 
 import { ImageUploader } from "../ImageUploader/ImageUploader";
+import { LargeModalDialog } from "../Modal/LargeModalDialog";
+import { ModalBody } from "../Modal/ModalBody";
+import { ModalFooter } from "../Modal/ModalFooter";
 
 export const UploadImageModal = ({setIsModalActive})=>{
 
@@ -36,13 +39,6 @@ export const UploadImageModal = ({setIsModalActive})=>{
         const imageKey = storeNewImage(imageInfo);
         navigate("/smartImageConfiguration/"+imageKey);
     }
-
-    const getClickedElementId = (event)=>{
-        if(event.target.id === "modalBackgroundDiv"){
-            setIsModalActive(false);
-        }
-    }
-
 
     useEffect(
         () =>{
@@ -86,28 +82,13 @@ export const UploadImageModal = ({setIsModalActive})=>{
     ,[])
 
     return(
-        <div
-            id="modalBackgroundDiv"
-            onClick={getClickedElementId}
-            className="modal fade show"
-            role="dialog"
-            tabIndex={-1}
-            style={
-                {
-                    display: "block",
-                    backgroundColor: "rgb(153,153,153,0.5)"
-                }
-            }
+        <LargeModalDialog
+            setIsModalActive={setIsModalActive}
+            modalTitle="Choose an image"
         >
-            <div className="modal-dialog modal-lg">
-                <div className="modal-content" style={{borderColor:"blue"}}>
-                    <div className="modal-header">
-                        <h5 className="modal-tilte">Upload image</h5>
-                        <button type="button" className="close close__button" onClick={()=>{setIsModalActive(false)}}>
-                            <span >x</span>
-                        </button>
-                    </div>
-                    <div className="modal-body" style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+            <>
+                <ModalBody>
+                    <>
                         <h2> Name of the image</h2>
                         <br/>
                         <input id={textInputId} type="text" placeholder="Image name" onChange={(event) => {setImageName(event.target.value);}} style={{minWidth : "30%"}}/>
@@ -118,18 +99,21 @@ export const UploadImageModal = ({setIsModalActive})=>{
                             file={file}
                             setFile={setFile}
                         />
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary"  onClick={()=>{setIsModalActive(false)}}>Close</button>
+                    </>
+                </ModalBody>
+                <ModalFooter
+                    setIsModalActive={setIsModalActive}
+                >
+                    <>
                         {
                             isInformationUploaded()
                                 &&
                             <button className="btn btn-primary" onClick={()=>{toNextStep()}}>Continue</button>
                         }
-                    </div>
-                </div>
-            </div>
-        </div>
+                    </>
+                </ModalFooter>
+            </>
+        </LargeModalDialog>
     )
 }
 
