4143d0741d0d459bc2f5c1e5c4d98bb03d1c4959
jaime <jaimegonicarnicero@hotmail.com>
2023-07-07 10:20:42 +0200

    Second try today

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename='/smart-image/'>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
